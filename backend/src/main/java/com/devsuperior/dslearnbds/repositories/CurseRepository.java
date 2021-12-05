3d72a0d79544419ebdb34681f1f839f5a8269be7
Fernando Coutinho <fa2c1@hotmail.com>
2021-12-05 10:55:28 -0300

    Repository, Exceptions, Validation, Security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/CurseRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import com.devsuperior.dslearnbds.entities.Course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurseRepository extends JpaRepository<Course, Long> {
+
+}
